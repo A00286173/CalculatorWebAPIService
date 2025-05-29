421345fa77bb7a27c5f8a85206cb92492230d85c
A00286173 <A00286173@mycambrian.ca>
2025-05-28 22:08:01 -0400

    Re-import project without .vs artifacts

---
+++ Interfaces/ICalculatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorWebApiProject.Interfaces
+{
+    public interface ICalculatorService<T>
+    {
+        T Add(T a, T b);
+        T Subtract(T a, T b);
+        T Multiply(T a, T b);
+        T Divide(T a, T b);
+    }
+}
