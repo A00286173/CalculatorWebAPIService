17a2048284c90cf8c7db3e1bb29dce99b83c9e18
A00286173 <A00286173@mycambrian.ca>
2025-05-28 22:29:25 -0400

    Final Project

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<ICalculatorService<double>, CalculatorService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
