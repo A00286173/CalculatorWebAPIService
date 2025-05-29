219bf3e40d56780d2e0f41cec239d6c3a9344b17
A00286173 <A00286173@mycambrian.ca>
2025-05-28 22:21:28 -0400

    Added Calculator Controller

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,47 @@
+﻿using CalculatorWebApiProject.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorWebApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ICalculatorService<double> _calculatorService;
+
+        public CalculatorController(ICalculatorService<double> calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
+        [HttpGet("add")]
+        public ActionResult<double> Add([FromQuery] double a, [FromQuery] double b)
+        {
+            double result = _calculatorService.Add(a, b);
+            return Ok(result);
+        }
+
+        [HttpGet("subtract")]
+        public ActionResult<double> Subtract([FromQuery] double a, [FromQuery] double b)
+        {
+            double result = _calculatorService.Subtract(a, b);
+            return Ok(result);
+        }
+
+        [HttpGet("multiply")]
+        public ActionResult<double> Multiply([FromQuery] double a, [FromQuery] double b)
+        {
+            double result = _calculatorService.Multiply(a, b);
+            return Ok(result);
+        }
+
+        [HttpGet("divide")]
+        public ActionResult<double> Divide([FromQuery] double a, [FromQuery] double b)
+        {
+            double result = _calculatorService.Divide(a, b);
+            return Ok(result);
+        }
+
+    }
+}
